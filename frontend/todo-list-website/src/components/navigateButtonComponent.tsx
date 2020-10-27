46df42cf8b4b5be25377e791ffadacbdbc0ad4c5
Nick Henneke <nickyhenneke@gmail.com>
2020-10-27 14:04:04 +0100

    Fixed the renders create button test

---
+++ frontend/todo-list-website/src/components/navigateButtonComponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export class NavigateButtonComponent extends React.Component {
+    render() {
+        // todo: styling, parameter for content (text), parameter to link to.
+        return <button></button>;
+    }
+}
