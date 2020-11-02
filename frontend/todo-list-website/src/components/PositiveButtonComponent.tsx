8214e1964c0d235c12025bad5273c5eb2bbcb048
Nick Henneke <nickyhenneke@gmail.com>
2020-11-02 13:53:05 +0100

    Implemented icon on PositiveButtonComponent and use PositiveButtonComponent in todoItemListPage

--- frontend/todo-list-website/src/components/PositiveButtonComponent.tsx
+++ frontend/todo-list-website/src/components/PositiveButtonComponent.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { Button } from '@material-ui/core'
+import {Button, Icon} from '@material-ui/core'
 
 interface ButtonProps {
     OnClick: (event: React.MouseEvent<HTMLButtonElement>) => void,
     Content: string,
-    // Icon: string, //todo
+    Icon?: object,
     TestID: string,
 }
 
 export function PositiveButtonComponent(Props: ButtonProps) {
-    // todo: styling
-    return <Button data-testid={Props.TestID} onClick={Props.OnClick}>{Props.Content}</Button>;
+    return <Button variant="contained" color={'primary'} startIcon={Props.Icon} data-testid={Props.TestID} onClick={Props.OnClick}>
+        <span className="mdc-button__label">{Props.Content}</span>
+    </Button>;
 }
--- frontend/todo-list-website/src/pages/TodoItemListPage.tsx
+++ frontend/todo-list-website/src/pages/TodoItemListPage.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import {PositiveButtonComponent} from "../components/PositiveButtonComponent";
+import {AddCircle} from '@material-ui/icons';
 
 export class TodoItemListPage extends React.Component {
     render() {
-        return <PositiveButtonComponent TestID={"navigate-to-create-page-button"} Content={"this is a button"} OnClick={ () => {} } />;
+        return <PositiveButtonComponent TestID={"navigate-to-create-page-button"} Icon={<AddCircle/>} Content={"Create"} OnClick={ () => {} } />;
     }
 }
