1cbe78deb08ded6f0ad02fa9eb44828cae20442e
Nick Henneke <nickyhenneke@gmail.com>
2020-11-02 13:59:55 +0100

    Refactored PositiveButtonComponent tests and added test case

--- frontend/todo-list-website/src/components/__tests__/PositiveButtonComponent.spec.tsx
+++ frontend/todo-list-website/src/components/__tests__/PositiveButtonComponent.spec.tsx
@@ -1,26 +1,30 @@
 import {render, screen} from "@testing-library/react";
 import {PositiveButtonComponent} from "../PositiveButtonComponent";
 import React from "react";
+import {AddCircle} from '@material-ui/icons';
+
+const handleClick = jest.fn()
+const expectedTestID = 'mock-test-id'
+const expectedContent = 'button content'
 
 test('renders without crashing', () => {
-    const handleClick = jest.fn()
-    const expectedContent = 'button content'
-    render(<PositiveButtonComponent OnClick={handleClick} Content={expectedContent}/>);
+    render(<PositiveButtonComponent OnClick={handleClick} TestID={expectedTestID} Content={expectedContent}/>);
+});
+
+test('renders with icon without crashing', async() => {
+    const expectedIcon = <AddCircle />
+    render(<PositiveButtonComponent Icon={expectedIcon} OnClick={handleClick} TestID={expectedTestID} Content={expectedContent}/>)
 });
 
 test('renders with content', async () => {
-    const handleClick = jest.fn()
-    const expectedContent = 'button content'
-    render(<PositiveButtonComponent OnClick={handleClick} Content={expectedContent}/>);
+    render(<PositiveButtonComponent OnClick={handleClick} TestID={expectedTestID} Content={expectedContent}/>);
     const button = await screen.findByText(expectedContent)
     expect(button).toBeTruthy();
-})
+});
 
 test('button click event works', async () => {
-    const handleClick = jest.fn()
-    const expectedContent = 'button content'
-    render(<PositiveButtonComponent OnClick={handleClick} Content={expectedContent}/>);
+    render(<PositiveButtonComponent OnClick={handleClick} TestID={expectedTestID} Content={expectedContent}/>);
     const button = await screen.findByText(expectedContent)
     button.click()
     expect(handleClick).toHaveBeenCalledTimes(1);
-})
+});
