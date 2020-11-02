34e3cc2ad666a97595cefd19c74a7def60fd2172
Nick Henneke <nickyhenneke@gmail.com>
2020-11-02 15:44:19 +0100

    Added tests (fail)

---
+++ frontend/todo-list-website/src/components/__tests__/HeaderComponent.spec.tsx
@@ -0,0 +1,14 @@
+import {render, screen} from "@testing-library/react"
+import {HeaderComponent} from "../HeaderComponent"
+
+const expectedHeader = "mock header"
+
+test('renders without crashing', () => {
+    render(<HeaderComponent Title={expectedHeader} />)
+})
+
+test( 'renders with expected title', async() => {
+    render(<HeaderComponent Title={expectedHeader} />);
+    const header = await screen.findByText(expectedHeader);
+    expect(header).toBeTruthy();
+})
